ff32eea23ef692bcd4db8fcdb2873e69f103d100
felipefialho <hi@felipefialho.com>
2023-07-28 17:58:56 -0300

    feat(linters): add plugin to use tokens to zindex

---
+++ packages/linters/src/stylelint/plugins/__tests__/use-tokens.spec.js
@@ -0,0 +1,94 @@
+const { messages } = require('../use-tokens')
+const stylelint = require('stylelint')
+
+const validCss = '.class { color: var(--color-primary); }'
+const invalidCss = '.class { color: #fff; }'
+
+const config = {
+  plugins: ['@juntossomosmais/linters/stylelint/plugins/use-tokens.js'],
+  rules: {
+    'plugin/use-tokens': true,
+  },
+}
+
+describe('use-tokens rule', () => {
+  it('should not working if plugin is not enabled', async () => {
+    const result = await stylelint.lint({
+      code: validCss,
+      config: {
+        plugins: ['@juntossomosmais/linters/stylelint/plugins/use-tokens.js'],
+        rules: { 'plugin/use-tokens': false },
+      },
+    })
+
+    expect(result.results[0].warnings).toHaveLength(0)
+  })
+
+  it('should accepts valid CSS', async () => {
+    const result = await stylelint.lint({
+      code: validCss,
+      config,
+    })
+
+    expect(result.results[0].warnings).toHaveLength(0)
+  })
+
+  it('should rejects invalid CSS', async () => {
+    const result = await stylelint.lint({
+      code: invalidCss,
+      config,
+    })
+
+    expect(result.results[0].warnings).toHaveLength(1)
+    expect(result.results[0].warnings[0].rule).toBe('plugin/use-tokens')
+    expect(result.results[0].warnings[0].severity).toBe('error')
+    expect(result.results[0].warnings[0].line).toBe(1)
+    expect(result.results[0].warnings[0].column).toBe(10)
+    expect(result.results[0].warnings[0].text).toBe(
+      messages.useToken({
+        tokenName: 'color-neutral-white',
+        tokenValue: '#fff',
+      })
+    )
+  })
+
+  it('should not report variables', async () => {
+    const result = await stylelint.lint({
+      code: '.class { color: var(--color-primary); }',
+      config,
+    })
+
+    expect(result.results[0].warnings).toHaveLength(0)
+  })
+
+  it('should not report when prop is CSS custom property', async () => {
+    const result = await stylelint.lint({
+      code: '.class { --color: #fff; }',
+      config,
+    })
+
+    expect(result.results[0].warnings).toHaveLength(0)
+  })
+
+  it('should not report when use zindex values as tokens', async () => {
+    const result = await stylelint.lint({
+      code: '.class { margin: 10; }',
+      config,
+    })
+
+    expect(result.results[0].warnings).toHaveLength(0)
+  })
+
+  it('should not report when use border-radius values as tokens but reporting if match with spacing values', async () => {
+    const result = await stylelint.lint({
+      code: '.class { border-radius: 8px; }',
+      config,
+    })
+    expect(result.results[0].warnings[0].text).not.toBe(
+      messages.useToken({ tokenName: 'border-radius', tokenValue: '4px' })
+    )
+    expect(result.results[0].warnings[0].text).toBe(
+      messages.useToken({ tokenName: 'spacing-xsmall', tokenValue: '8px' })
+    )
+  })
+})
