020ddc9215ee2d33bdaf32ce92ea07350932fe0c
felipefialho <hi@felipefialho.com>
2023-09-14 10:58:52 -0300

    feat(linters): add rule to avoid specifying units is zero-values

--- packages/linters/src/stylelint.config.js
+++ packages/linters/src/stylelint.config.js
@@ -29,5 +29,6 @@ module.exports = {
     'declaration-block-single-line-max-declarations': 1,
     'declaration-block-semicolon-newline-after': 'always-multi-line',
     'rule-empty-line-before': 'always',
+    'length-zero-no-unit': true,
   },
 }
