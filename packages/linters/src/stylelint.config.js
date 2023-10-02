dbe55191a2617dcb541a05d3828ae5ff7a2066cd
felipefialho <hi@felipefialho.com>
2023-10-02 09:58:56 -0300

    fix(linters): remove duplicated rule

--- packages/linters/src/stylelint.config.js
+++ packages/linters/src/stylelint.config.js
@@ -28,7 +28,6 @@ module.exports = {
     ],
     'declaration-block-single-line-max-declarations': 1,
     'declaration-block-semicolon-newline-after': 'always-multi-line',
-    'rule-empty-line-before': 'always',
     'length-zero-no-unit': true,
   },
 }
