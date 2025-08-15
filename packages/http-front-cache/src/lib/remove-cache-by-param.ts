5ad83d33101d4e0f3ba99482b1fd09cdaf347ab5
tassioFront <tassio72@hotmail.com>
2025-08-15 14:41:21 -0300

    feat: new cache assignment logic
    BREAKING CHANGE: cache key now uses function name, anonymous functions are not supported

--- packages/http-front-cache/src/lib/remove-cache-by-param.ts
+++ packages/http-front-cache/src/lib/remove-cache-by-param.ts
@@ -1,17 +1,19 @@
 import hash from 'object-hash'
 
-import { Provider } from './types'
+import { Provider, ServiceFunction } from './types'
 
 /**
  * Delete cache by param (where param is the same param provided to the serviceFunction)
+ * @param serviceFunctionName: a unique string to identify the cache origin (e.g., service name)
  * @param params: the parameters that will be used find the cache key (hash)
  * @param provider: the provider used to store the cache (such as in-memory, session storage, etc)
  */
-export const removeCacheByParam = (
+export function removeCacheByParam(
   provider: Provider,
+  serviceFunctionName: string,
   ...params: unknown[]
-): void | null => {
-  const key = hash(params)
+): void | null {
+  const key = hash([serviceFunctionName, params])
   const cachedEntry = provider.getItem(key)
 
   if (!cachedEntry) {
@@ -19,4 +21,19 @@ export const removeCacheByParam = (
   }
 
   provider.removeItem(key)
+
+  return undefined
+}
+
+/**
+ * Creates a cache remover bound to a service function, using its name as cacheAssignment.
+ * @param provider The cache provider (e.g., sessionStorageProvider)
+ * @param serviceFunction The service function whose cache you want to remove
+ */
+export function removeCacheByParamFactory<TParams extends unknown[], TResult>(
+  provider: Provider,
+  serviceFunction: ServiceFunction<TParams, TResult>
+) {
+  return (...params: TParams) =>
+    removeCacheByParam(provider, serviceFunction.name, ...params)
 }
