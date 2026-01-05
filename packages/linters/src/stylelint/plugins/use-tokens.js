66faf9cf728e8e014edbbfc3faf7578abbb06853
Igor Wessel <igor.silva@juntossomosmais.com.br>
2026-01-05 09:58:57 -0300

    chore: add jsdoc for type annotation

--- packages/linters/src/stylelint/plugins/use-tokens.js
+++ packages/linters/src/stylelint/plugins/use-tokens.js
@@ -20,56 +20,58 @@ const meta = {
   schema: [],
 }
 
-module.exports = stylelint.createPlugin(
-  ruleName,
-  (primaryOption, _, context) => {
-    return function (root, result) {
-      const validOptions = stylelint.utils.validateOptions(result, ruleName, {
-        actual: primaryOption,
-      })
+/**
+ * @type {import('stylelint').Rule}
+ */
+const ruleFunction = (primaryOption, _, context) => {
+  return function (root, result) {
+    const validOptions = stylelint.utils.validateOptions(result, ruleName, {
+      actual: primaryOption,
+    })
 
-      if (!validOptions) return
+    if (!validOptions) return
 
-      root.walkDecls((decl) => {
-        if (decl.prop.startsWith('--')) return
+    root.walkDecls((decl) => {
+      if (decl.prop.startsWith('--')) return
 
-        Object.entries(tokens).forEach(([tokenName, tokenValue]) => {
-          const specificTokens = ['zindex', 'border-radius']
-          const isSpecificTokens = specificTokens.some((output) =>
-            tokenName.includes(output)
-          )
+      Object.entries(tokens).forEach(([tokenName, tokenValue]) => {
+        const specificTokens = ['zindex', 'border-radius']
+        const isSpecificTokens = specificTokens.some((output) =>
+          tokenName.includes(output)
+        )
 
-          const regexPattern = new RegExp(
-            `(^|\\s)(${tokens[tokenName]}|#${
-              tokens[tokens[tokenName]]
-            })(\\s|;|$)`,
-            'g'
-          )
+        const regexPattern = new RegExp(
+          `(^|\\s)(${tokens[tokenName]}|#${
+            tokens[tokens[tokenName]]
+          })(\\s|;|$)`,
+          'g'
+        )
 
-          if (!isSpecificTokens && regexPattern.test(decl.value)) {
-            const fix = () => {
-              decl.value = `var(--${tokenName})`
-            }
-
-            //TODO: in stylelint@16 should remove context.fix and pass `fix` callback to report function. https://stylelint.io/developer-guide/rules#context
-            if (context.fix) {
-              fix()
-
-              return
-            }
-
-            stylelint.utils.report({
-              message: messages.useToken({ tokenName, tokenValue }),
-              node: decl,
-              result,
-              ruleName,
-            })
+        if (!isSpecificTokens && regexPattern.test(decl.value)) {
+          const fix = () => {
+            decl.value = `var(--${tokenName})`
           }
-        })
+
+          //TODO: in stylelint@16 should remove context.fix and pass `fix` callback to report function. https://stylelint.io/developer-guide/rules#context
+          if (context.fix) {
+            fix()
+
+            return
+          }
+
+          stylelint.utils.report({
+            message: messages.useToken({ tokenName, tokenValue }),
+            node: decl,
+            result,
+            ruleName,
+          })
+        }
       })
-    }
+    })
   }
-)
+}
+
+module.exports = stylelint.createPlugin(ruleName, ruleFunction)
 
 module.exports.ruleName = ruleName
 module.exports.messages = messages
